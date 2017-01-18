ceaaa8fb874fb9bdccca7ad6a599326855dfbf50
Carter Turnbaugh <carturn@nuevaschool.org>
2017-01-17 21:08:57 -0800

    Fixed double deleting issue
    This caused crashes on OSX, but not Linux.

--- lidar_grapher/doubly_linked_list.cpp
+++ lidar_grapher/doubly_linked_list.cpp
@@ -1,36 +1,48 @@
 #include "doubly_linked_list.h"
-
+#include <iostream>
 
 void lidar_datapoint_list_cleanup(doubly_linked_list_node<lidar_datapoint> * first_node){
-        doubly_linked_list_node<lidar_datapoint> * node = first_node;
+        doubly_linked_list_node<lidar_datapoint> * node = first_node->next;
 
 	bool finished = false;
 
         while(node != NULL && !finished){
-		delete node->prev->data;
-		delete node->prev;
+		delete node->data;
 		node = node->next;
 		if(node == first_node){
 			finished = true;
 		}
+		else{
+			delete node->prev;
+		}
         }
+
+	delete node->data;
+	delete node->prev;
+	delete node;
 
         first_node = NULL;
 }
 
 void line_list_cleanup(doubly_linked_list_node<line> * first_node){
-        doubly_linked_list_node<line> * node = first_node;
+        doubly_linked_list_node<line> * node = first_node->next;
 
 	bool finished = false;
 
         while(node != NULL && !finished){
-		delete node->prev->data;
-		delete node->prev;
+		delete node->data;
 		node = node->next;
 		if(node == first_node){
 			finished = true;
 		}
+		else{
+			delete node->prev;
+		}
         }
+
+	delete node->data;
+	delete node->prev;
+	delete node;
 
         first_node = NULL;
 }
